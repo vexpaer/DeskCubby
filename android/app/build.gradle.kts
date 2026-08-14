01ace0c80823ebf1a55cbd8be31bc3fb551982e4
vexpaer <2161884681@qq.com>
2026-08-14 19:39:19 +0800

    release: Android 0.16.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.deskcubby.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
-        versionName = "0.16.2"
+        versionCode = 35
+        versionName = "0.16.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
