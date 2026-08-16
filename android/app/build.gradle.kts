9bf9a0b4cb012de5dda44397d17812804e05f270
vexpaer <2161884681@qq.com>
2026-08-16 12:28:26 +0800

    release: Android 0.19.1
    fix: release meal capture state after durable write
    - versionCode 43 / versionName 0.19.1
    - CHANGELOG updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.deskcubby.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "0.19.0"
+        versionCode = 43
+        versionName = "0.19.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
